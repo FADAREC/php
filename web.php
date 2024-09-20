f220d38192c9c2b5d756c1417b19e91bffece6d9
FADAREC <134747489+FADAREC@users.noreply.github.com>
2024-09-20 15:16:01 +0100

    Update web.php

--- web.php
+++ web.php
@@ -1,13 +1,6 @@
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
-   Route::post('register', [AuthController::class, 'register']);
-   Route::post('login', [AuthController::class, 'login']);
-   Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
-   Route::middleware('auth:sanctum')->group(function () {
-       Route::post('posts', [PostController::class, 'store']);
-       Route::put('posts/{id}', [PostController::class, 'update']);
-       Route::delete('posts/{id}', [PostController::class, 'destroy']);
-   });
 
-   Route::get('posts', [PostController::class, 'index']);
-   Route::get('posts/{id}', [PostController::class, 'show']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->get('me', [AuthController::class, 'me']);
